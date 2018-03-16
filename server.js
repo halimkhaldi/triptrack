2c023beee06f042e60eef8a2b89a0a8c729a221e
Abdelhalim <golden-boy1994@hotmail.fr>
2018-03-16 16:57:32 -0400

    intiiale file strucutre

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express=require('express');
-var app=express();
-var db=require('./models/index.js');
+var express = require('express');
+var app = express();
+var db = require('./models/index.js');
 var bodyParser = require('body-parser');
 
 // Configure app
@@ -8,6 +8,10 @@ app.set('views', __dirname + '/views');      // Views directory
 app.use(express.static('public'));          // Static directory
 app.use(bodyParser.urlencoded({ extended: true })); // req.body
 
+// change engine
+app.set("view engine", "ejs");
+// methode overider
+app.use(methodOverride("_method"));
 // Set CORS Headers
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');

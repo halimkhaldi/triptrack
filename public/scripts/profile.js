25248397ab16fcac472b53db83b243435016b87e
camario25 <camario25@yahoo.com>
2018-03-19 14:57:37 -0700

    got header to show on profile view, fixed button

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,34 +1,27 @@
-
-$(document).ready(function() {
-  console.log("Let's get coding!");
-  $.ajax({
-        method: 'GET',
-        url: '/users/:id'
-        success: function(json) {
-          // console.log(`sent back ${json.metadata.count}`);
-          findFeature(json);
-          mapMarker(json);
-          // console.log(json.features[0].properties.title)
-          // $("#info").html(json.metadata.count);
-        },
-        error: function() {
-          alert('There was an error getting profile');
-        }
-        // beforeSend: function () {
-        //   $('#page').append('Loading');
-        // },
-        // complete: function () {
-        //   $('#loading').remove();
-        // }
-  });
-  var findFeature = function (json) {
-      console.log(json.metadata.count);
-    $(json.features).each(function (index, feature) {
-        var titles = feature.properties.title;
-        console.log(titles);
-        console.log(index);
-      $('#info').append(`<p>${titles}<p>`);
-
-    });
-  };
-});
+// 
+// $(document).ready(function() {
+//   console.log("Let's get coding!");
+//   $.ajax({
+//         method: 'GET',
+//         url: '/users/:id'
+//         success: function(json) {
+//           // console.log(`sent back ${json.metadata.count}`);
+//           findFeature(json);
+//           // console.log(json.features[0].properties.title)
+//           // $("#info").html(json.metadata.count);
+//         },
+//         error: function() {
+//           alert('There was an error getting profile');
+//         }
+//   });
+//   var findFeature = function (json) {
+//       console.log(json.metadata.count);
+//     $(json.features).each(function (index, feature) {
+//         var titles = feature.properties.title;
+//         console.log(titles);
+//         console.log(index);
+//       $('#info').append(`<p>${titles}<p>`);
+// 
+//     });
+//   };
+// });

3da0f6ea2739e86b1d8ca5803cc2ae1aa2ede1d8
fchow2018 <dong_chow@yahoo.com>
2018-03-19 17:39:20 -0700

    Almost finished with search page.

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,33 +1,88 @@
-service = new google.maps.places.PlacesService(map);
-service.nearbySearch(request, callback);
+function initAutocomplete() {
 
-var map;
-var service;
-var infowindow;
+        var map;
+        var map_id;
+        function map(){
+          // get cookies for lat and lng
+          //document.cookie is an array of strings
+          var x = document.cookie.split(';');
+          console.log(x);
+            var lat = x[0].split('=');
+            var lng = x[1].split('=');
 
-function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+            map = new google.maps.Map(map_id, {
+                center: { lat: parseFloat(lat[1]), lng: parseFloat(lng[1]) },
+                zoom: 15
+              });
+              var center = new google.maps.Marker({
+                position: { lat: parseFloat(lat[1]), lng: parseFloat(lng[1]) },
+                map: map,
+                title: 'Your actual position'
+              });
+        }
 
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
+        $(document).ready(function(){
+        map_id = document.getElementById('map');
+        map();
+        });
 
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    type: ['restaurant']
-  };
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
-}
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+        new google.maps.places.initAutocomplete(document.getElementById('initAutocomplete'));
+      }
